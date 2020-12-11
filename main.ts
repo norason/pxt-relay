22fa6cb2919389b488316c998b63a4afb7c7ceed
nora <norason@gmail.com>
2020-12-11 18:09:59 +0800

    0.3

--- main.ts
+++ main.ts
@@ -1,14 +1,6 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
     /**
-    * 計算長方形面積，並回傳
-    */
-    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
-    //% blockGap=2 weight=0 blockExternalInputs=true
-    //export function areaOfRectangle(length: number, width:number): number {
-    //    return length*width
-    //}
-    /**
     * 計算長方形面積，不回傳，只顯示在LED
     */
     //% blockId="writeRelay" block="Pin Number of Relay %pinNumber|switch %status"
